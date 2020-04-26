0af4daedc8ce94b137ae63b885984d322c0f8324
Deijai <djairn18@gmail.com>
2020-04-26 20:20:12 -0300

    subindo o projeto

---
+++ src/main/java/com/dev/deijai/api/SisMiranteApplication.java
@@ -0,0 +1,13 @@
+package com.dev.deijai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisMiranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisMiranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/deijai/api/SisMiranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.deijai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisMiranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
